24fe4f2ef99d93d33a84f572dde63d077b533dbd
sevanrit <sevanrit@gmail.com>
2024-10-07 20:00:25 +0300

    no bugs and nothing

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "djoser",
-    "src.profiles"
+    "src.profiles",
+    #"src.profiles.apps.ProfilesConfig"
 ]
 
 MIDDLEWARE = [
