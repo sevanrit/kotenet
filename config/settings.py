17b223bd71b61e2ec2355a1a7d8df5675d4df1d0
sevanrit <sevanrit@gmail.com>
2024-10-07 22:41:20 +0300

    stable DB VPS version

--- config/settings.py
+++ config/settings.py
@@ -91,12 +91,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        #"ENGINE": os.environ.get("POSTGRES_ENGINE", "django.db.backends.sqlite3"),
+        #"NAME": os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
+        "ENGINE": os.environ.get("POSTGRES_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("POSTGRES_DB", "kotenet"),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
@@ -154,4 +156,4 @@ SIMPLE_JWT = {
    #'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-#AUTH_USER_MODEL = 'src.profiles.UserNet'
+AUTH_USER_MODEL = 'profiles.UserNet'
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class UserNet(AbstractBaseUser):
+class UserNet(AbstractUser):
     """Custom User Model"""
+    #username = models.CharField(max_length=50, unique=True)  # Поле для username
+
     middle_name = models.CharField(max_length=50)
     first_login = models.DateTimeField(null=True)
     phone = models.CharField(max_length=14)
     avatar = models.ImageField(upload_to='user/avatar/', blank=True, null=True)
 
+    #USERNAME_FIELD = 'username'  # Поле для аутентификации
